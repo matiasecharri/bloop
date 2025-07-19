a2956eaf30d445a9d9815907934f13eae6517491
matiasecharri <matiasecharri@hotmail.com>
2025-07-19 16:14:39 -0300

    feat: tabs

--- src/app/features/animation-dashboard/sidebar/Sidebar.tsx
+++ src/app/features/animation-dashboard/sidebar/Sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { ReactNode, useEffect, useState } from "react";
 import {
   AnimationPicker,
   BackgroundPicker,
@@ -6,21 +8,41 @@ import {
 } from "./components";
 import s from "./Sidebar.module.css";
 
+interface TabType {
+  tabName: string;
+  component: ReactNode;
+}
+
+const tabs: TabType[] = [
+  { tabName: "text", component: <TextPicker /> },
+  { tabName: "animation", component: <AnimationPicker /> },
+  { tabName: "color", component: <ColorPicker /> },
+  { tabName: "background", component: <BackgroundPicker /> },
+];
+
 const Sidebar = () => {
+  const [selectedTab, setSelectedTab] = useState<any>();
+
+  useEffect(() => {
+    console.log(selectedTab);
+  }, [selectedTab]);
+
   return (
     <aside className={s.sidebar}>
       <section className={s.buttonBar}>
-        <button>TX</button>
-        <button>FX</button>
-        <button>CO</button>
-        <button>BG</button>
-        <button>EX</button>
+        {tabs.map((tab) => (
+          <button
+            key={tab.tabName}
+            onClick={(e) => setSelectedTab(() => tab.tabName)}
+          >
+            {tab.tabName}
+          </button>
+        ))}
       </section>
       <section className={s.currentOptions}>
-        <TextPicker />
-        <AnimationPicker />
+        {/* <AnimationPicker />
         <ColorPicker />
-        <BackgroundPicker />
+        <BackgroundPicker /> */}
       </section>
     </aside>
   );
