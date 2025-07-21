13ff71ea40b480f305d5fd94c48da919f496ae0a
Diana <dianavodopivec16@gmail.com>
2025-07-21 17:11:05 -0300

    feat(animations-dashboard): add context + reducer

--- src/app/features/animation-dashboard/context/Controls.tsx
+++ src/app/features/animation-dashboard/context/Controls.tsx
@@ -1,36 +1,82 @@
-import { createContext, ReactNode, useContext } from "react";
-import { ControlsSettings, DefaultControlsSettings } from "./controlsTypes";
+import { createContext, ReactNode, useContext, useReducer } from "react";
+import {
+  AnimationSettings,
+  ControlsSettings,
+  DefaultControlsSettings,
+  TextSettings,
+} from "./controlsTypes";
 
+//REDUCER TYPES
+export const CONTROLS_ACTIONS = {
+  TEXT: "TEXT",
+  ANIMATIONS: "ANIMATIONS",
+};
+
+export type ControlsAction =
+  | { type: typeof CONTROLS_ACTIONS.TEXT; payload: TextSettings }
+  | { type: typeof CONTROLS_ACTIONS.ANIMATIONS; payload: AnimationSettings };
+
+//REDUCER
+const controlReducer = (
+  state: ControlsSettings,
+  action: ControlsAction
+): ControlsSettings => {
+  switch (action.type) {
+    case CONTROLS_ACTIONS.TEXT:
+      return {
+        ...state,
+        text: {
+          ...state.text,
+          ...action.payload,
+        },
+      };
+
+    case CONTROLS_ACTIONS.ANIMATIONS:
+      return {
+        ...state,
+        animations: {
+          ...state.animations,
+          ...action.payload,
+        },
+      };
+
+    default:
+      return state;
+  }
+};
+
+//CONTEXT TYPES
 interface ControlsProviderProps {
   children: ReactNode;
 }
 
-interface ControlsValueType {
-  settings: ControlsSettings;
-  setSettings: () => void;
+interface ControlsContextValue {
+  state: ControlsSettings;
+  dispatch: React.Dispatch<ControlsAction>;
 }
 
-const ControlsContext = createContext<ControlsValueType | undefined>(undefined);
+//CONTEXT
+const ControlsContext = createContext<ControlsContextValue | undefined>(
+  undefined
+);
 
 export const useControls = () => {
   const context = useContext(ControlsContext);
 
   if (context === undefined)
     throw new Error(
-      "useControls hook must be initialized inside of a <Controls/> Provider"
+      "useControls must be called inside of a <ControlsProvider/>"
     );
 
   return context;
 };
 
+//PROVIDER + REDUCER
 export const ControlsProvider = ({ children }: ControlsProviderProps) => {
+  const [state, dispatch] = useReducer(controlReducer, DefaultControlsSettings);
+
   return (
-    <ControlsContext.Provider
-      value={{
-        settings: DefaultControlsSettings,
-        setSettings: () => console.log("Controls Settings"),
-      }}
-    >
+    <ControlsContext.Provider value={{ state, dispatch }}>
       {children}
     </ControlsContext.Provider>
   );
