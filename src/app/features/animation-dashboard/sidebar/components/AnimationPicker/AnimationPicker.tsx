413872aef499261583abc690d79e682e2c024b88
matiasecharri <matiasecharri@hotmail.com>
2025-07-21 23:15:16 -0300

    feat(draft): add animation state values

--- src/app/features/animation-dashboard/sidebar/components/AnimationPicker/AnimationPicker.tsx
+++ src/app/features/animation-dashboard/sidebar/components/AnimationPicker/AnimationPicker.tsx
@@ -2,13 +2,13 @@ import { useControls } from "../../../context";
 
 const AnimationPicker = () => {
   const { state } = useControls();
+
   const { animations } = state;
 
   return (
     <section>
       <p>Duration: {animations.duration}</p>
-      <p>End Delay: {animations.endDelay}</p>
-      <p>Start Delay:{animations.startDelay}</p>
+      <p>Delay: {animations.delay}</p>
       <p>FadeIn: {animations.fadeIn ? "true" : "false"}</p>
       <p>FadeOut: {animations.fadeOut ? "true" : "false"}</p>
       <p>Loop: {animations.loop ? "true" : "false"}</p>
