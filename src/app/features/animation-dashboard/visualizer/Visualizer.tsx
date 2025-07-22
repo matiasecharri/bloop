1056bcc54bc31f6f3c67d4f5c6f46c29c06dc5ec
matiasecharri <matiasecharri@hotmail.com>
2025-07-21 23:11:58 -0300

    feat: add gsap

--- src/app/features/animation-dashboard/visualizer/Visualizer.tsx
+++ src/app/features/animation-dashboard/visualizer/Visualizer.tsx
@@ -1,18 +1,39 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import { TextSettings, useControls } from "../context";
+import { useRef } from "react";
 import s from "./Visualizer.module.css";
 
 const Visualizer = () => {
   const { state } = useControls();
-  const { text } = state;
+  const { text, animations } = state;
+
+  const textRef = useRef<HTMLParagraphElement>(null);
 
   const dynamicStyles: Omit<TextSettings, "userText"> = {
     fontSize: text.fontSize,
+    fontWeight: text.fontWeight,
     fontFamily: text.fontFamily,
   };
 
+  useGSAP(() => {
+    const text = textRef.current;
+
+    gsap.to(text, {
+      opacity: 0,
+      ease: "power1",
+      duration: animations.duration,
+      yoyo: true,
+      delay: animations.delay,
+      repeat: animations.loop ? -1 : 0,
+    });
+  }, [animations]);
+
   return (
     <section className={s.visualizer}>
-      <p style={dynamicStyles}>{text.userText}</p>
+      <p style={dynamicStyles} ref={textRef}>
+        {text.userText}
+      </p>
     </section>
   );
 };
