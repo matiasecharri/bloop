d424da8c7abb1941a2b3fad78dc839037f8b683c
matiasecharri <matiasecharri@hotmail.com>
2025-07-24 14:56:25 -0300

    feat: add RangeWrapper component

---
+++ src/shared/components/molecules/range-wrapper/RangeWrapper.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import s from "./RangeWrapper.module.css";
+
+interface RangeWrapperProps {
+  children: ReactNode;
+}
+
+const RangeWrapper = ({ children }: RangeWrapperProps) => {
+  return <div className={s.wrapper}>{children}</div>;
+};
+
+export default RangeWrapper; 
